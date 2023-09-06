72c98b8969deacdbae32704a7bd0318dcebabe88
wytxer <wy@bszhct.com>
2023-09-06 18:23:57 +0800

    feat：新增必要的公众号接口

--- lib/wechat-official-account.interface.ts
+++ lib/wechat-official-account.interface.ts
@@ -5,6 +5,72 @@ export interface IAccessTokenResponse extends IStateResponse {
   expires_in: number
 }
 
+export interface IQrCodeResponse {
+  ticket: string
+  expire_seconds: number
+  url: string
+}
+
+export interface IQueryUserInfoResponse {
+  // 用户是否订阅该公众号标识，值为 0 时，代表此用户没有关注该公众号，拉取不到其余信息
+  subscribe: number
+  // 用户的标识，对当前公众号唯一
+  openid: string
+  // 用户的语言，简体中文为 zh_CN
+  language: string
+  // 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+  subscribe_time: number
+  // 只有在用户将公众号绑定到微信开放平台账号后，才会出现该字段
+  unionid: string
+  // 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+  remark: string
+  // 用户所在的分组 ID
+  groupid: string
+  // 用户被打上的标签 ID 列表
+  tagid_list: string[]
+  /**
+   * 返回用户关注的渠道来源
+   * ADD_SCENE_SEARCH：公众号搜索
+   * ADD_SCENE_ACCOUNT_MIGRATION：公众号迁移
+   * ADD_SCENE_PROFILE_CARD：名片分享
+   * ADD_SCENE_QR_CODE：扫描二维码
+   * ADD_SCENE_PROFILE_LINK：图文页内名称点击
+   * ADD_SCENE_PROFILE_ITEM：图文页右上角菜单
+   * ADD_SCENE_PAID：支付后关注
+   * ADD_SCENE_WECHAT_ADVERTISEMENT：微信广告
+   * ADD_SCENE_REPRINT：他人转载
+   * ADD_SCENE_LIVESTREAM：视频号直播
+   * ADD_SCENE_CHANNELS：视频号
+   * ADD_SCENE_OTHERS：其他
+   */
+  subscribe_scene:
+    | 'ADD_SCENE_SEARCH'
+    | 'ADD_SCENE_ACCOUNT_MIGRATION'
+    | 'ADD_SCENE_PROFILE_CARD'
+    | 'ADD_SCENE_QR_CODE'
+    | 'ADD_SCENE_PROFILE_LINK'
+    | 'ADD_SCENE_PROFILE_ITEM'
+    | 'ADD_SCENE_PAID'
+    | 'ADD_SCENE_WECHAT_ADVERTISEMENT'
+    | 'ADD_SCENE_REPRINT'
+    | 'ADD_SCENE_LIVESTREAM'
+    | 'ADD_SCENE_CHANNELS'
+    | 'ADD_SCENE_OTHERS'
+  // 二维码扫码场景（开发者自定义）
+  qr_scene: string
+  // 二维码扫码场景描述（开发者自定义）
+  qr_scene_str: string
+}
+
+export interface ITagsResponse extends IStateResponse {
+  // 标签列表
+  tags: {
+    id: number
+    name: string
+    count: number
+  }[]
+}
+
 export interface IQrCodeParams {
   access_token: string
   expire_seconds: number
@@ -12,4 +78,57 @@ export interface IQrCodeParams {
   action_info: {
     [key: string]: any
   }
+}
+
+export interface IRemarkParams {
+  access_token: string
+  openid: string
+  remark: string
+}
+
+export interface IUsersAddTagParams {
+  access_token: string
+  tagid: number
+  openid_list: string[]
+}
+
+type ButtonType =
+  | 'view'
+  | 'click'
+  | 'miniprogram'
+  | 'location_select'
+  | 'media_id'
+  | 'view_limited'
+  | 'article_id'
+  | 'article_view_limited'
+
+export interface ICreateMenusParams {
+  access_token: string
+  button: {
+    // 菜单的响应动作类型，view 表示网页类型，click 表示点击类型，miniprogram 表示小程序类型
+    type?: ButtonType
+    // 菜单标题，不超过 16 个字节，子菜单不超过 60 个字节
+    name: string
+    // 菜单 KEY 值，用于消息接口推送，不超过 128 字节，click 等点击类型必须
+    key?: string
+    // 跳转外部链接
+    url?: string
+    // 跳转小程序
+    appid?: string
+    pagepath?: string
+    // 图文消息
+    media_id?: string
+    // 发布后的图文消息
+    article_id?: string
+    sub_button?: {
+      type: ButtonType
+      name: string
+      key?: string
+      url?: string
+      appid?: string
+      pagepath?: string
+      media_id?: string
+      article_id?: string
+    }[]
+  }[]
 }
--- lib/wechat-official-account.ts
+++ lib/wechat-official-account.ts
@@ -1,7 +1,16 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
-import { IWechatConfig } from './wechat.interface'
-import { IAccessTokenResponse, IQrCodeParams } from './wechat-official-account.interface'
+import { IWechatConfig, IStateResponse } from './wechat.interface'
+import {
+  IAccessTokenResponse,
+  IQrCodeResponse,
+  IQueryUserInfoResponse,
+  ITagsResponse,
+  IQrCodeParams,
+  IRemarkParams,
+  IUsersAddTagParams,
+  ICreateMenusParams
+} from './wechat-official-account.interface'
 
 export class WechatOfficialAccount {
   constructor(readonly config: IWechatConfig, private readonly apiUrl: string = 'https://api.weixin.qq.com') {
@@ -13,6 +22,8 @@ export class WechatOfficialAccount {
     }
     this.config = config
   }
+
+  static qrCodeAddress = 'https://mp.weixin.qq.com/cgi-bin/showqrcode'
 
   async request(config: AxiosRequestConfig) {
     config.url = this.apiUrl + config.url
@@ -29,7 +40,7 @@ export class WechatOfficialAccount {
 
     return await this.request({
       method: 'get',
-      url: `/cgi-bin/token?appid=?appid=${appid}&secret=${secret}&grant_type=client_credential`
+      url: `/cgi-bin/token?appid=${appid}&secret=${secret}&grant_type=client_credential`
     })
   }
 
@@ -38,11 +49,11 @@ export class WechatOfficialAccount {
    * @link https://developers.weixin.qq.com/doc/offiaccount/Account_Management/Generating_a_Parametric_QR_Code.html
    * @returns
    */
-  async qrCode(params: IQrCodeParams): Promise<IAccessTokenResponse> {
+  async qrCode(params: IQrCodeParams): Promise<IQrCodeResponse> {
     const { access_token, expire_seconds, action_name, action_info } = params
 
     return await this.request({
-      method: 'get',
+      method: 'post',
       url: `/cgi-bin/qrcode/create?access_token=${access_token}`,
       data: {
         expire_seconds,
@@ -57,9 +68,78 @@ export class WechatOfficialAccount {
    * @link https://developers.weixin.qq.com/doc/offiaccount/User_Management/Get_users_basic_information_UnionID.html#UinonId
    * @returns
    */
-  async queryUserInfo(accessToken: string, openId: string): Promise<IAccessTokenResponse> {
+  async queryUserInfo(accessToken: string, openId: string): Promise<IQueryUserInfoResponse> {
     return await this.request({
       url: `/cgi-bin/user/info?access_token=${accessToken}&openid=${openId}&lang=zh_CN`
+    })
+  }
+
+  /**
+   * 设置用户备注名
+   * @link https://developers.weixin.qq.com/doc/offiaccount/User_Management/Configuring_user_notes.html
+   * @returns
+   */
+  async remark(params: IRemarkParams): Promise<IStateResponse> {
+    return await this.request({
+      method: 'post',
+      url: `/cgi-bin/user/info/updateremark?access_token=${params.access_token}`,
+      data: {
+        openid: params.openid,
+        remark: params.remark
+      }
+    })
+  }
+
+  /**
+   * 获取公众号已创建的标签
+   * @link https://developers.weixin.qq.com/doc/offiaccount/User_Management/User_Tag_Management.html
+   * @returns
+   */
+  async tags(accessToken: string): Promise<ITagsResponse> {
+    return await this.request({
+      method: 'get',
+      url: `/cgi-bin/tags/get?access_token=${accessToken}`
+    })
+  }
+
+  /**
+   * 批量为用户打标签
+   * @link https://developers.weixin.qq.com/doc/offiaccount/User_Management/User_Tag_Management.html
+   * @returns
+   */
+  async usersAddTag(params: IUsersAddTagParams): Promise<IStateResponse> {
+    return await this.request({
+      method: 'post',
+      url: `/cgi-bin/tags/members/batchtagging?access_token=${params.access_token}`,
+      data: {
+        tagid: params.tagid,
+        openid_list: params.openid_list
+      }
+    })
+  }
+
+  /**
+   * 创建自定义菜单
+   * @link https://developers.weixin.qq.com/doc/offiaccount/Custom_Menus/Creating_Custom-Defined_Menu.html
+   * @returns
+   */
+  async createMenus(params: ICreateMenusParams): Promise<IStateResponse> {
+    return await this.request({
+      method: 'post',
+      url: `/cgi-bin/menu/create?access_token=${params.access_token}`,
+      data: { button: params.button }
+    })
+  }
+
+  /**
+   * 获取自定义菜单配置
+   * @link https://developers.weixin.qq.com/doc/offiaccount/Custom_Menus/Getting_Custom_Menu_Configurations.html
+   * @returns
+   */
+  async getMenus(accessToken: string): Promise<any> {
+    return await this.request({
+      method: 'get',
+      url: `/cgi-bin/menu/get?access_token=${accessToken}`
     })
   }
 }
